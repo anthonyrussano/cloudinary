136a14a04e72a3a03f681f84c98c2e0002a7c9e9
Anthony Russano <me@anthonyrussano.com>
2023-12-02 22:21:30 -0800

    preserve file-name

--- app.py
+++ app.py
@@ -13,23 +13,26 @@ cloudinary.config(
     api_secret=vars.CLOUDINARY_API_SECRET,
 )
 
+
 def create_uploads_folder():
     # Create the "uploads" folder if it doesn't exist
     folder_path = "uploads"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
+
 def determine_resource_type(file_extension):
     # Define a basic mapping of file extensions to resource types
-    image_extensions = {'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'}
-    video_extensions = {'mp4', 'avi', 'mov', 'flv', 'wmv'}
+    image_extensions = {"jpg", "jpeg", "png", "gif", "bmp", "tiff"}
+    video_extensions = {"mp4", "avi", "mov", "flv", "wmv"}
 
     if file_extension in image_extensions:
-        return 'image'
+        return "image"
     elif file_extension in video_extensions:
-        return 'video'
+        return "video"
     else:
         return None  # Unknown type or unsupported
+
 
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
@@ -47,14 +50,21 @@ def upload_file():
 
             if resource_type:
                 # Upload the file to Cloudinary
-                response = upload(file_path, resource_type=resource_type)
+                response = upload(
+                    file_path,
+                    resource_type=resource_type,
+                    use_filename=True,
+                    unique_filename=False,
+                )
 
                 if "url" in response:
                     # Delete the local file after upload (optional)
                     os.remove(file_path)
 
                     mime_type = file.mimetype
-                    return render_upload_response(resource_type, response['url'], mime_type)
+                    return render_upload_response(
+                        resource_type, response["url"], mime_type
+                    )
                 else:
                     return "Upload failed."
             else:
@@ -62,8 +72,9 @@ def upload_file():
 
     return render_template("index.html")
 
+
 def render_upload_response(resource_type, url, mime_type):
-    if resource_type == 'video':
+    if resource_type == "video":
         return f"""
             <h1>Upload successful!</h1>
             <video controls>
@@ -73,7 +84,7 @@ def render_upload_response(resource_type, url, mime_type):
             <p>URL: <a href="{url}">{url}</a></p>
             <p><a href="/">Upload another file</a></p>
         """
-    elif resource_type == 'image':
+    elif resource_type == "image":
         return f"""
             <h1>Upload successful!</h1>
             <img src="{url}" alt="Uploaded image">
@@ -82,6 +93,7 @@ def render_upload_response(resource_type, url, mime_type):
         """
     else:
         return "Error in rendering upload response."
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
